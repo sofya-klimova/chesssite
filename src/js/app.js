2d692baf1e289a1b0d5789785cdb0884da0a09ca
sofya-klimova <firsttingakvey@gmail.com>
2024-11-24 22:44:27 +0300

    Add files via upload

---
+++ src/js/app.js
@@ -0,0 +1,7 @@
+import * as flsFunctions from "./modules/functions.js";
+flsFunctions.isWebp();
+import {slider} from './files/sliders.js';
+import {submenuActive} from './files/script.js';
+import { clicker } from './files/forms.js';
+import { useDynamicAdapt } from './files/dynamicAdapt.js';
+useDynamicAdapt();
